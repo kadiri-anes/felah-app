56ea74b4db770dd42658944d2b090ed3f0ab3e8e
kadiri-anes <kadirianes90@gmail.com>
2026-09-07 21:10:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -450,7 +450,7 @@ st.markdown(
     }}
 
     .block-container {{
-        padding-top: 1.5rem !important;
+        padding-top: 3rem !important;
         padding-bottom: 1rem !important;
         padding-left: 1rem !important;
         padding-right: 1rem !important;
@@ -637,7 +637,7 @@ st.markdown(
     /* Mobile Responsive Scaling (< 640px): KEEP TITLE AND 3 TABS CENTERED */
     @media (max-width: 640px) {{
         .block-container {{
-            padding-top: 1rem !important;
+            padding-top: 2rem !important;
             padding-left: 0.55rem !important;
             padding-right: 0.55rem !important;
             padding-bottom: 0.7rem !important;
